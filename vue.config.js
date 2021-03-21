fc1cc4ef51669ee45cb381c972bf44381ded2fbd
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-22 00:50:12 +0200

    battle-city-mp: resolve @-relative modules

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+const path = require('path');
+
 module.exports = {
     pages: {
         index: {
             entry: 'client/main.ts',
             template: 'client/index.html',
             filename: 'index.html',
+        },
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname),
+            },
         },
     },
     chainWebpack: config => {
