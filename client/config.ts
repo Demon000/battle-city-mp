fc7bb60d483a4de636c733e7eff1e951f9d3e447
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-24 11:01:03 +0200

    battle-city-mp: add more config options and move configuration to common place

--- client/config.ts
+++
@@ -1,2 +0,0 @@
-export const CONFIG_SOCKET_BASE_URL: string = process.env
-    .VUE_APP_SOCKET_BASE_URL as string;
---
+++ config.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+export const CLIENT_CONFIG_SOCKET_BASE_URL: string = process.env
+    .VUE_APP_SOCKET_BASE_URL as string;
+// eslint-disable-next-line
+export const CLIENT_CONFIG_FPS: number = Number.parseInt(process.env
+    .VUE_APP_CLIENT_FPS!);
+// eslint-disable-next-line
+export const CLIENT_CONFIG_VISIBLE_GAME_SIZE: number = Number.parseInt(process.env
+    .VUE_APP_VISIBLE_GAME_SIZE!);
+// eslint-disable-next-line
+export const SERVER_CONFIG_TPS: number = Number.parseInt(process.env
+    .SERVER_TPS!);
