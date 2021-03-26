5972a66d6e0d673b56d206f4364aa7de1f24916e
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-26 21:50:24 +0200

    battle-city-mp: add object type and tank tier enums

---
+++ object/GameObjectType.ts
@@ -0,0 +1,16 @@
+export enum GameShortObjectType {
+    BRICK_WALL = 'B',
+    STEEL_WALL = 'S',
+    PLAYER_SPAWN = 'O',
+    LEVEL_BORDER = '#',
+}
+
+export enum GameObjectType {
+    ANY = 'any',
+    BRICK_WALL = 'brick-wall',
+    STEEL_WALL = 'steel-wall',
+    PLAYER_SPAWN = 'player-spawn',
+    LEVEL_BORDER = 'level-border',
+    TANK = 'tank',
+    BULLET = 'bullet',
+}
---
+++ tank/TankTier.ts
@@ -0,0 +1,6 @@
+export enum TankTier {
+    PLAYER_TIER_1 = 'player-tier-1',
+    PLAYER_TIER_2 = 'player-tier-2',
+    PLAYER_TIER_3 = 'player-tier-3',
+    PLAYER_TIER_4 = 'player-tier-4',
+}
