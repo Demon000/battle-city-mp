bd7d01088cfde20e92db68b2ee5ce3c52b076980
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-26 12:10:32 +0200

    battle-city-mp: load initial objects and players from server before listening to any other event

--- game/GameClientSocket.ts
+++ game/GameClientSocket.ts
@@ -8,6 +8,7 @@ import { GameEvent } from './GameEvent';
 export default class GameClientSocket {
     socket;
     gameClient;
+    initialized = false;
 
     constructor(socket: Socket, gameClient: GameClient) {
         this.socket = socket;
@@ -17,33 +18,67 @@ export default class GameClientSocket {
             console.log('Connected');
         });
 
-        this.socket.on(GameEvent.OBJECT_REGISTERED, (objectOptions: GameObjectOptions) => {
-            const object = GameObjectFactory.buildFromOptions(objectOptions);
-            this.gameClient.onObjectRegisteredOnServer(object);
-        });
+        this.initialize();
+    }
 
-        this.socket.on(GameEvent.PLAYER_ADDED, (playerOptions: PlayerOptions) => {
-            const player = new Player(playerOptions);
-            this.gameClient.onPlayerAddedOnServer(player);
-        });
+    initialize(): void {
+        let gameObjectsLoaded = false;
+        let playersLoaded = false;
 
-        this.socket.on(GameEvent.PLAYER_CHANGED, (playerOptions: PlayerOptions) => {
-            const player = new Player(playerOptions);
-            this.gameClient.onPlayerChangedOnServer(player);
-        });
+        const checkIfInitialized = () => {
+            if (!gameObjectsLoaded || !playersLoaded) {
+                return;
+            }
 
-        this.socket.on(GameEvent.PLAYER_REMOVED, (playerId: string) => {
-            this.gameClient.onPlayerRemovedOnServer(playerId);
-        });
+            this.initialized = true;
+        }
 
         this.socket.emit(GameEvent.GET_GAME_OBJECTS, (objectOptions: GameObjectOptions[]) => {
             const objects = objectOptions.map(o => GameObjectFactory.buildFromOptions(o));
             this.gameClient.onObjectsRegisteredOnServer(objects);
+            checkIfInitialized();
         });
 
         this.socket.emit(GameEvent.GET_PLAYERS, (playerOptions: PlayerOptions[]) => {
             const players = playerOptions.map(p => new Player(p));
             this.gameClient.onPlayersAddedOnServer(players);
+            checkIfInitialized();
         });
+    }
+
+    registerListeners(): void {
+        const runIfInitialized = <T extends Function>(fn: T) => {
+            if (this.initialized) {
+                fn();
+            }
+        };
+
+        this.socket.on(GameEvent.PLAYER_ADDED, (playerOptions: PlayerOptions) => runIfInitialized(() => {
+            const player = new Player(playerOptions);
+            this.gameClient.onPlayerAddedOnServer(player);
+        }));
+
+        this.socket.on(GameEvent.PLAYER_CHANGED, (playerOptions: PlayerOptions) => runIfInitialized(() => {
+            const player = new Player(playerOptions);
+            this.gameClient.onPlayerChangedOnServer(player);
+        }));
+
+        this.socket.on(GameEvent.PLAYER_REMOVED, (playerId: string) => runIfInitialized(() => {
+            this.gameClient.onPlayerRemovedOnServer(playerId);
+        }));
+
+        this.socket.on(GameEvent.OBJECT_REGISTERED, (objectOptions: GameObjectOptions) => runIfInitialized(() => {
+            const object = GameObjectFactory.buildFromOptions(objectOptions);
+            this.gameClient.onObjectRegisteredOnServer(object);
+        }));
+
+        this.socket.on(GameEvent.OBJECT_CHANGED, (objectOptions: GameObjectOptions) => runIfInitialized(() => {
+            const object = GameObjectFactory.buildFromOptions(objectOptions);
+            this.gameClient.onObjectChangedOnServer(object);
+        }));
+
+        this.socket.on(GameEvent.OBJECT_UNREGISTERED, (objectId: number) => runIfInitialized(() => {
+            this.gameClient.onObjectUnregisteredOnServer(objectId);
+        }));
     }
 }
