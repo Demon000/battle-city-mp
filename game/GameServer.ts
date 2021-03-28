9200bf6193c73a657a9b990b9262cb8375efc631
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-28 18:15:47 +0300

    battle-city-mp: get rid of useless function declarations when only used once, attach all event handlers in constructor

--- game/GameServer.ts
+++ game/GameServer.ts
@@ -43,163 +43,179 @@ export default class GameServer {
         this.playerService = new PlayerService(this.playerRepository);
         this.ticker = new Ticker(128);
 
-        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_WALL, this.onBulletWillHitWall, this);
-        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_TANK, this.onBulletWillHitTank, this);
-        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_BULLET, this.onBulletWillHitBullet, this);
+        /**
+         * GameMapService event handlers
+         */
+        this.gameMapService.emitter.on(GameMapServiceEvent.OBJECTS_SPAWNED,
+            (objects: GameObject[]) => {
+                this.gameObjectService.registerObjects(objects);
+            });
 
-        this.gameMapService.emitter.on(GameMapServiceEvent.OBJECTS_SPAWNED, this.onObjectsSpawned, this);
+        /**
+         * PlayerService event handlers
+         */
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REQUESTED_GAME_OBJECTS,
+            (playerId: string) => {
+                const objects = this.gameObjectService.getObjects();
+                this.emitter.emit(GameEvent.PLAYER_OBJECTS_REGISTERD, playerId, objects);
+            });
 
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REQUESTED_GAME_OBJECTS, this.onPlayerRequestedGameObjects, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REQUESTED_PLAYERS, this.onPlayerRequestedPlayers, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REQUESTED_PLAYERS,
+            (playerId: string) => {
+                const players = this.playerService.getPlayers();
+                this.emitter.emit(GameEvent.PLAYER_PLAYERS_ADDED, playerId, players);
+            });
 
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_ADDED, this.onPlayerAdded, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_CHANGED, this.onPlayerChanged, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REMOVED, this.onPlayerRemoved, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_ADDED,
+            (player: Player) => {
+                this.emitter.emit(GameEvent.PLAYER_ADDED, player);
+            });
 
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_SHOOTING, this.onPlayerShooting, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_MOVING, this.onPlayerMoving, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_NOT_MOVING, this.onPlayerNotMoving, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_CHANGED,
+            (player: Player) => {
+                this.emitter.emit(GameEvent.PLAYER_CHANGED, player);
+            });
 
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REQUESTED_DIRECTION, this.onObjectRequestedDirection, this);
-        this.collisionService.emitter.on(CollisionServiceEvent.OBJECT_DIRECTION_ALLOWED, this.onObjectDirectionAllowed, this);
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REQUESTED_POSITION, this.onObjectRequestedPosition, this);
-        this.collisionService.emitter.on(CollisionServiceEvent.OBJECT_POSITION_ALLOWED, this.onObjectPositionAllowed, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_REMOVED,
+            (playerId: string) => {
+                this.emitter.emit(GameEvent.PLAYER_REMOVED, playerId);
+            });
 
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_BOUNDING_BOX_CHANGED, this.onObjectBoundingBoxChanged, this);
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REGISTERED, this.onObjectRegistered, this);
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_UNREGISTERED, this.onObjectUnregistered, this);
-        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_CHANGED, this.onObjectChanged, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_SHOOTING,
+            (playerId: string) => {
+                // spawn bullet using tank position
+                console.log('player shooting ' + playerId);
+            });
 
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_SPAWN_TANK_REQUESTED, this.onPlayerSpawnTankRequested, this);
-        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_DESPAWN_TANK_REQUESTED, this.onPlayerDespawnTankRequested, this);
-        this.tankService.emitter.on(TankServiceEvent.PLAYER_TANK_CHANGED, this.onPlayerTankChanged, this);
-        this.tankService.emitter.on(TankServiceEvent.TANK_SPAWNED, this.onObjectSpawned, this);
-        this.tankService.emitter.on(TankServiceEvent.TANK_DESPAWNED, this.onObjectDespawned, this);
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_MOVING,
+            (playerId: string, direction: Direction) => {
+                const tankId = this.playerService.getPlayerTankId(playerId);
+                if (tankId === undefined) {
+                    return;
+                }
 
-        this.ticker.emitter.on(TickerEvent.TICK, this.onTick, this);
+                this.gameObjectService.processObjectDirection(tankId, direction);
+                this.gameObjectService.setObjectMoving(tankId, true);
+            });
+
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_NOT_MOVING,
+            (playerId: string) => {
+                const tankId = this.playerService.getPlayerTankId(playerId);
+                if (tankId === undefined) {
+                    return;
+                }
+    
+                this.gameObjectService.setObjectMoving(tankId, false);
+            });
+
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_SPAWN_TANK_REQUESTED,
+            (playerId: string) => {
+                const spawnPosition = this.gameObjectService.getRandomSpawnPosition();
+                this.tankService.spawnPlayerTank(playerId, spawnPosition);
+            });
+        this.playerService.emitter.on(PlayerServiceEvent.PLAYER_DESPAWN_TANK_REQUESTED,
+            (playerId: string, tankId: number) => {
+                this.tankService.despawnPlayerTank(playerId, tankId);
+            });
+
+        /**
+         * GameObjectService event handlers
+         */
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REQUESTED_DIRECTION,
+            (objectId: number, direction: Direction) => {
+                this.collisionService.validateObjectDirection(objectId, direction);
+            });
+
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REQUESTED_POSITION,
+            (objectId: number, position: Point) => {
+                this.collisionService.validateObjectMovement(objectId, position);
+            });
+
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_BOUNDING_BOX_CHANGED,
+            (objectId: number) => {
+                this.collisionService.updateObjectCollisions(objectId);
+            });
+
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_REGISTERED,
+            (object: GameObject) => {
+                this.collisionService.registerObjectCollisions(object.id);
+                this.emitter.emit(GameEvent.OBJECT_REGISTERED, object);
+            });
+
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_UNREGISTERED,
+            (objectId: number) => {
+                this.collisionService.unregisterObjectCollisions(objectId);
+                this.emitter.emit(GameEvent.OBJECT_UNREGISTERED, objectId);
+            });
+
+        this.gameObjectService.emitter.on(GameObjectServiceEvent.OBJECT_CHANGED,
+            (object: GameObject) => {
+                this.emitter.emit(GameEvent.OBJECT_CHANGED, object);
+            });
+
+        /**
+         * TankService event handlers
+         */
+        this.tankService.emitter.on(TankServiceEvent.PLAYER_TANK_CHANGED,
+            (playerId: string, tankId: number) => {
+                this.playerService.setPlayerTankId(playerId, tankId);
+            });
+
+        this.tankService.emitter.on(TankServiceEvent.TANK_SPAWNED,
+            (object: GameObject) => {
+                this.gameObjectService.registerObject(object);
+            });
+
+        this.tankService.emitter.on(TankServiceEvent.TANK_DESPAWNED,
+            (objectId: number) => {
+                this.gameObjectService.unregisterObject(objectId);
+            });
+
+        /**
+         * CollisionService event handlers
+         */
+        this.collisionService.emitter.on(CollisionServiceEvent.OBJECT_DIRECTION_ALLOWED,
+            (objectId: number, direction: Direction) => {
+                this.gameObjectService.setObjectDirection(objectId, direction);
+            });
+        this.collisionService.emitter.on(CollisionServiceEvent.OBJECT_POSITION_ALLOWED,
+            (objectId: number, position: Point) => {
+                this.gameObjectService.setObjectPosition(objectId, position);
+            });
+
+        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_WALL,
+            (movingObjectId: number, position: Point, staticObjectId: number) => {
+                console.log('Bullet hit wall');
+            });
+
+        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_TANK,
+            (movingObjectId: number, position: Point, staticObjectId: number) => {
+                console.log('Bullet hit tank');
+            });
+
+        this.collisionService.emitter.on(CollisionEventType.BULLET_HIT_BULLET,
+            (movingObjectId: number, position: Point, staticObjectId: number) => {
+                console.log('Bullet hit bullet');
+            });
+
+        /**
+         * Ticker event handlers
+         */
+        this.ticker.emitter.on(TickerEvent.TICK,
+            (deltaSeconds: number) => {
+                this.playerService.processPlayerStatus();
+                this.gameObjectService.processObjectsMovement(deltaSeconds);
+            });
 
         this.gameMapService.loadFromFile('./maps/simple.json');
-    }
-
-    getGameObjects(): GameObject[] {
-        return this.gameObjectService.getObjects();
-    }
-
-    getPlayers(): Player[] {
-        return this.playerService.getPlayers();
-    }
-    
-    onBulletWillHitWall(movingObjectId: number, position: Point, staticObjectId: number): void {
-        console.log('Bullet hit wall');
-    }
-
-    onBulletWillHitTank(movingObjectId: number, position: Point, staticObjectId: number): void {
-        console.log('Bullet hit tank');
-    }
-
-    onBulletWillHitBullet(movingObjectId: number, position: Point, staticObjectId: number): void {
-        console.log('Bullet hit bullet');
-    }
-
-    onPlayerMoving(playerId: string, direction: Direction): void {
-        const tankId = this.playerService.getPlayerTankId(playerId);
-        if (tankId === undefined) {
-            return;
-        }
-
-        this.gameObjectService.processObjectDirection(tankId, direction);
-        this.gameObjectService.setObjectMoving(tankId, true);
-    }
-
-    onPlayerNotMoving(playerId: string): void {
-        const tankId = this.playerService.getPlayerTankId(playerId);
-        if (tankId === undefined) {
-            return;
-        }
-
-        this.gameObjectService.setObjectMoving(tankId, false);
-    }
-
-    onObjectRequestedDirection(objectId: number, direction: Direction): void {
-        this.collisionService.validateObjectDirection(objectId, direction);
-    }
-
-    onObjectDirectionAllowed(objectId: number, direction: Direction): void {
-        this.gameObjectService.setObjectDirection(objectId, direction);
-    }
-
-    onObjectRequestedPosition(objectId: number, position: Point): void {
-        this.collisionService.validateObjectMovement(objectId, position);
-    }
-
-    onObjectPositionAllowed(objectId: number, position: Point): void {
-        this.gameObjectService.setObjectPosition(objectId, position);
-    }
-
-    onObjectBoundingBoxChanged(objectId: number): void {
-        this.collisionService.updateObjectCollisions(objectId);
-    }
-
-    onObjectChanged(object: GameObject): void {
-        this.emitter.emit(GameEvent.OBJECT_CHANGED, object);
-    }
-
-    onObjectsSpawned(objects: GameObject[]): void {
-        this.gameObjectService.registerObjects(objects);
-    }
-
-    onObjectSpawned(object: GameObject): void {
-        this.gameObjectService.registerObject(object);
-    }
-
-    onObjectRegistered(object: GameObject): void {
-        this.collisionService.registerObjectCollisions(object.id);
-        this.emitter.emit(GameEvent.OBJECT_REGISTERED, object);
-    }
-
-    onObjectDespawned(objectId: number): void {
-        this.gameObjectService.unregisterObject(objectId);
-    }
-
-    onObjectUnregistered(objectId: number): void {
-        this.collisionService.unregisterObjectCollisions(objectId);
-        this.emitter.emit(GameEvent.OBJECT_UNREGISTERED, objectId);
-    }
-
-    onPlayerShooting(playerId: string): void {
-        // const tank = this.tankService.getPlayerTank(player);
-        // spawn bullet using tank position
-        console.log('player shooting ' + playerId);
-    }
-
-    onPlayerAdded(player: Player): void {
-        this.emitter.emit(GameEvent.PLAYER_ADDED, player);
-    }
-
-    onPlayerChanged(player: Player): void {
-        this.emitter.emit(GameEvent.PLAYER_CHANGED, player);        
-    }
-
-    onPlayerRemoved(playerId: string): void {
-        this.emitter.emit(GameEvent.PLAYER_REMOVED, playerId);
     }
 
     onPlayerRequestedGameObjectsFromClient(playerId: string): void {
         this.playerService.requestPlayerGameObjects(playerId);
     }
 
-    onPlayerRequestedGameObjects(playerId: string): void {
-        const objects = this.gameObjectService.getObjects();
-        this.emitter.emit(GameEvent.PLAYER_OBJECTS_REGISTERD, playerId, objects);
-    }
-
     onPlayerRequestedPlayersFromClient(playerId: string): void {
         this.playerService.requestPlayerPlayers(playerId);
-    }
-
-    onPlayerRequestedPlayers(playerId: string): void {
-        const players = this.playerService.getPlayers();
-        this.emitter.emit(GameEvent.PLAYER_PLAYERS_ADDED, playerId, players);
     }
 
     onPlayerActionFromClient(playerId: string, action: Action): void {
@@ -219,23 +235,5 @@ export default class GameServer {
     onPlayerDisconnectedFromClient(playerId: string): void {
         this.playerService.requestPlayerSpawnStatus(playerId, PlayerSpawnStatus.DESPAWN);
         this.playerService.requestPlayerDisconnect(playerId);
-    }
-
-    onPlayerSpawnTankRequested(playerId: string): void {
-        const spawnPosition = this.gameObjectService.getRandomSpawnPosition();
-        this.tankService.spawnPlayerTank(playerId, spawnPosition);
-    }
-
-    onPlayerDespawnTankRequested(playerId: string, tankId: number): void {
-        this.tankService.despawnPlayerTank(playerId, tankId);
-    }
-
-    onPlayerTankChanged(playerId: string, tankId: number): void {
-        this.playerService.setPlayerTankId(playerId, tankId);
-    }
-
-    onTick(deltaSeconds: number): void {
-        this.playerService.processPlayerStatus();
-        this.gameObjectService.processObjectsMovement(deltaSeconds);
     }
 }
