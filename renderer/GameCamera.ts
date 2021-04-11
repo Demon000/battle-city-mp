a4f5f5646583d18bd5a0f15a653c7de227fb06eb
Cosmin Tanislav <demonsingur@gmail.com>
2021-04-11 18:48:39 +0300

    Revert "battle-city-mp: try to implement camera smoothing"
    This reverts commit 16eca21328b78357ef8242ab265b330e41516b40.

--- renderer/GameCamera.ts
+++ renderer/GameCamera.ts
@@ -1,56 +1,20 @@
-import GameObject from '@/object/GameObject';
-import { Direction } from '@/physics/Direction';
 import Point from '@/physics/point/Point';
 
 export default class GameCamera {
-    private lastUpdateSeconds?: number;
-    private lastPosition?: Point;
-    watchedObject?: GameObject;
+    position?: Point;
 
-    getCurrentSeconds(): number {
-        return Date.now() / 1000;
-    }
-
-    setWatchedObject(object: GameObject | undefined): void {
-        this.watchedObject = object;
-        this.updateWatchedPosition();
-    }
-
-    updateWatchedPosition(): void {
-        if (this.watchedObject === undefined) {
-            return;
-        }
-
-        this.lastPosition = this.watchedObject.centerPosition;
-        this.lastUpdateSeconds = this.getCurrentSeconds();
+    setPosition(position: Point): void {
+        this.position = position;
     }
 
     getPosition(): Point | undefined {
-        const currentSeconds = this.getCurrentSeconds();
-        if (this.lastUpdateSeconds === undefined || this.lastPosition === undefined) {
+        if (this.position === undefined) {
             return undefined;
         }
 
-        if (this.watchedObject === undefined) {
-            return this.lastPosition;
-        }
-
-        console.log(currentSeconds, this.lastUpdateSeconds);
-        const distance = this.watchedObject.movementSpeed * (currentSeconds - this.lastUpdateSeconds);
-        console.log(distance);
-        if (this.watchedObject.direction === Direction.UP) {
-            this.lastPosition.y -= distance;
-        } else if (this.watchedObject.direction === Direction.RIGHT) {
-            this.lastPosition.x += distance;
-        } else if (this.watchedObject.direction === Direction.DOWN) {
-            this.lastPosition.y += distance;
-        } else if (this.watchedObject.direction === Direction.LEFT) {
-            this.lastPosition.x -= distance;
-        }
-
         return {
-            x: Math.floor(this.lastPosition.x),
-            y: Math.floor(this.lastPosition.y),
+            x: Math.floor(this.position.x),
+            y: Math.floor(this.position.y),
         };
     }
 }
