d800fe46d44e861fe0fb47fe9846f814c6437afe
Cosmin Tanislav <demonsingur@gmail.com>
2021-05-27 17:17:49 +0300

    battle-city-mp: add missing flag service

---
+++ flag/FlagService.ts
@@ -0,0 +1,40 @@
+import GameObject from '@/object/GameObject';
+import { GameObjectType } from '@/object/GameObjectType';
+import MapRepository from '@/utils/MapRepository';
+import { EventEmitter } from 'eventemitter3';
+import Flag, { FlagType, PartialFlagOptions } from './Flag';
+
+export enum FlagServiceEvent {
+    FLAG_UPDATED = 'flag-updated',
+}
+
+interface FlagServiceEvents {
+    [FlagServiceEvent.FLAG_UPDATED]: (flagId: number, options: PartialFlagOptions) => void,
+}
+
+export default class FlagService {
+    private repository;
+    emitter = new EventEmitter<FlagServiceEvents>();
+
+    constructor(repository: MapRepository<number, GameObject>) {
+        this.repository = repository;
+    }
+
+    getFlag(flagId: number): Flag {
+        const object = this.repository.get(flagId);
+        if (object.type !== GameObjectType.FLAG) {
+            throw new Error('Game object type is not flag');
+        }
+
+        return object as Flag;
+    }
+
+    setFlagType(flagId: number, type: FlagType): void {
+        const flag = this.getFlag(flagId);
+        flag.flagType  = type;
+
+        this.emitter.emit(FlagServiceEvent.FLAG_UPDATED, flagId, {
+            flagType: type,
+        });
+    }
+}
