547655645b72dddb8a0b0e3169de2aeb4a254708
Cosmin Tanislav <demonsingur@gmail.com>
2021-06-25 23:28:43 +0300

    battle-city-mp: enrich config util

--- config/Config.ts
+++ config/Config.ts
@@ -4,7 +4,7 @@ import jp from 'jsonpath';
 import JSON5 from 'json5';
 
 export class Config {
-    private nameToData = new Map<string, any>();
+    private nameToData = {} as Record<string, any>;
 
     loadAll(dirPath: string): void {
         const files = fs.readdirSync(dirPath);
@@ -19,17 +19,37 @@ export class Config {
         }
     }
 
-    get<T>(name: string, path: string): T {
-        const data = this.nameToData.get(name);
+    getData(name: string): any {
+        const data = this.nameToData[name];
         if (data === undefined) {
             throw new Error(`Invalid configuration name '${name}'`);
         }
 
+        return data;
+    }
+
+    getDataMultiple(names: Array<string>): Record<string, any> {
+        const data = {};
+
+        for (const name of names) {
+            const data = this.getData(name);
+            this.nameToData[name] = data;
+        }
+
+        return data;
+    }
+
+    getDataPath<T>(data: any, path: string): T {
         const value = jp.value(data, path);
         if (value === undefined) {
             throw new Error(`Invalid value for path '${path}'`);
         }
 
-        return value as T;
+        return value;
+    }
+
+    get<T>(name: string, path: string): T {
+        const data = this.getData(name);
+        return this.getDataPath(name, path);
     }
 }
