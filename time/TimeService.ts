c13c6674f208e1baa4d67bdbca39177a51595bb6
Cosmin Tanislav <demonsingur@gmail.com>
2021-06-27 11:23:44 +0300

    battle-city-mp: refactor time service to properly handle restarts

--- time/TimeService.ts
+++ time/TimeService.ts
@@ -27,7 +27,13 @@ export class TimeService {
         private config: Config,
     ) {}
 
-    setMapVotePassed(value: boolean): void {
+    isMapVoteTime(): boolean {
+        const mapVoteSeconds = this.config.get<number>('time', 'mapVoteSeconds');
+        return this.roundSeconds <= mapVoteSeconds;
+    }
+
+    updateMapVotePassed(): void {
+        const value = this.isMapVoteTime();
         if (this.mapVotePassed === value) {
             return;
         }
@@ -36,13 +42,26 @@ export class TimeService {
         this.emitter.emit(TimeServiceEvent.MAP_VOTE_TIME, value);
     }
 
-    setScoreboardWatchPassed(value: boolean): void {
+    isScoreboardWatchTime(): boolean {
+        const scoreboardWatchSeconds = this.config.get<number>('time', 'scoreboardWatchSeconds');
+        return this.roundSeconds <= scoreboardWatchSeconds;
+    }
+
+    updateScoreboardWatchPassed(): void {
+        const value = this.isScoreboardWatchTime();
         if (this.scoreboardWatchPassed === value) {
             return;
         }
 
         this.scoreboardWatchPassed = value;
         this.emitter.emit(TimeServiceEvent.SCOREBOARD_WATCH_TIME, value);
+    }
+
+    triggerRoundRestart(): void {
+        const roundTimeSeconds = this.config.get<number>('time', 'roundTimeSeconds');
+        if (this.roundSeconds === roundTimeSeconds) {
+            this.emitter.emit(TimeServiceEvent.ROUND_TIME_RESTART);
+        }
     }
 
     setRoundSeconds(roundSeconds: number): void {
@@ -52,22 +71,21 @@ export class TimeService {
 
         this.roundSeconds = roundSeconds;
         this.emitter.emit(TimeServiceEvent.ROUND_TIME_UPDATED, roundSeconds);
+
+        this.updateMapVotePassed();
+        this.updateScoreboardWatchPassed();
+        this.triggerRoundRestart();
     }
 
     setRoundTime(roundTime: number): void {
         const roundSeconds = Math.floor(roundTime);
         this.setRoundSeconds(roundSeconds);
-
         this.roundTime = roundTime;
-
-        this.setMapVotePassed(this.isMapVoteTime());
-        this.setScoreboardWatchPassed(this.isScoreboardWatchTime());
     }
 
     restartRoundTime(): void {
         const roundTimeSeconds = this.config.get<number>('time', 'roundTimeSeconds');
         this.setRoundTime(roundTimeSeconds);
-        this.emitter.emit(TimeServiceEvent.ROUND_TIME_RESTART);
     }
 
     decreaseRoundTime(deltaSeconds: number): void {
@@ -76,15 +94,5 @@ export class TimeService {
 
     isRoundEnded(): boolean {
         return this.roundTime <= 0;
-    }
-
-    isMapVoteTime(): boolean {
-        const mapVoteSeconds = this.config.get<number>('time', 'mapVoteSeconds');
-        return this.roundTime <= mapVoteSeconds;
-    }
-
-    isScoreboardWatchTime(): boolean {
-        const scoreboardWatchSeconds = this.config.get<number>('time', 'scoreboardWatchSeconds');
-        return this.roundTime <= scoreboardWatchSeconds;
     }
 }
