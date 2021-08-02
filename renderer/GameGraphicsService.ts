30c3a55e6448e11edd5a1233f756f54418597357
Cosmin Tanislav <demonsingur@gmail.com>
2021-08-03 01:00:34 +0300

    battle-city-mp: upsert dirty grapgics processing

--- renderer/GameGraphicsService.ts
+++ renderer/GameGraphicsService.ts
@@ -174,6 +174,6 @@ export class GameGraphicsService {
             return;
         }
 
-        entity.addComponent(DirtyGraphicsComponent);
+        entity.upsertComponent(DirtyGraphicsComponent);
     }
 }
