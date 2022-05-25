84c18bcb336a235924ac9371b3bcd0aa9d3a6ebd
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-25 22:33:53 +0300

    battle-city-mp: implement lazy iterable flattening

--- utils/LazyIterable.ts
+++ utils/LazyIterable.ts
@@ -1,6 +1,7 @@
 export enum IterableOperationType {
     MAP,
     FILTER,
+    FLATTEN,
 }
 
 export type IterableOperation = {
@@ -9,28 +10,64 @@ export type IterableOperation = {
 } | {
     type: IterableOperationType.FILTER;
     fn: (value: any) => boolean;
+} | {
+    type: IterableOperationType.FLATTEN;
 };
 
 export class LazyIterableIterator<T> {
     private iterator;
+    private currentIterator: Iterator<any>;
     private operations;
+    private start;
 
-    constructor(iterator: Iterator<any>, operations: IterableOperation[]) {
+    constructor(
+        iterator: Iterator<any>,
+        operations: IterableOperation[],
+        start: number,
+    ) {
         this.iterator = iterator;
+        this.currentIterator = iterator;
         this.operations = operations;
+        this.start = start;
     }
 
     next(): IteratorResult<T> {
-        let item;
-        while (!(item = this.iterator.next()).done) {
-            let isFilteredOut = false;
-            let result: any = item.value;
+        while (true) {
+            const item = this.currentIterator.next();
 
-            for (const operation of this.operations) {
+            /*
+             * Current inner iterator is done, switch to main one.
+             */
+            if (item.done && this.currentIterator !== this.iterator) {
+                this.currentIterator = this.iterator;
+                continue;
+            }
+
+            if (item.done || this.currentIterator !== this.iterator) {
+                return item;
+            }
+
+            let result: any = item.value;
+            let isFilteredOut = false;
+
+            for (let i = this.start; i < this.operations.length; i++) {
+                const operation = this.operations[i];
+
                 if (operation.type === IterableOperationType.FILTER) {
                     isFilteredOut = !operation.fn(result);
                 } else if (operation.type === IterableOperationType.MAP) {
                     result = operation.fn(result);
+                } else if (operation.type === IterableOperationType.FLATTEN) {
+                    /*
+                     * Flatten can only be called if the source is an iterable,
+                     * wrap it in a LazyIterable, and start its inner operations
+                     * from the operation after the current flatten one.
+                     * Force exit the operations loop.
+                     */
+                    result = LazyIterable.from(
+                        result as Iterable<any>, this.operations, i + 1);
+                    this.currentIterator = result[Symbol.iterator]();
+                    isFilteredOut = true;
                 }
 
                 if (isFilteredOut) {
@@ -47,29 +84,36 @@ export class LazyIterableIterator<T> {
                 done: false,
             };
         }
-
-        return {
-            value: undefined,
-            done: true,
-        };
     }
 }
 
-export class LazyIterable<T> implements Iterable<T> {
+export class LazyIterable<T, X = T extends Iterable<infer V> ? V : never> implements Iterable<T> {
     private iterable;
     private operations;
+    private start;
 
-    private constructor(iterable: Iterable<any>, operations: IterableOperation[] = []) {
+    private constructor(
+        iterable: Iterable<any>,
+        operations: IterableOperation[] = [],
+        start = 0,
+    ) {
         this.iterable = iterable;
         this.operations = operations;
+        this.start = start;
     }
 
-    static from<B>(iterable: Iterable<B>): LazyIterable<B> {
-        if (iterable instanceof LazyIterable) {
+    static from<B>(
+        iterable: Iterable<B>,
+        operations?: IterableOperation[],
+        start?: number,
+    ): LazyIterable<B> {
+        if ((operations === undefined || operations.length === 0)
+            && (start === undefined || start === 0)
+            && iterable instanceof LazyIterable) {
             return iterable;
         }
 
-        return new LazyIterable<B>(iterable);
+        return new LazyIterable<B>(iterable, operations, start);
     }
 
     map<R, C>(
@@ -92,6 +136,12 @@ export class LazyIterable<T> implements Iterable<T> {
         }]);
     }
 
+    flatten(): LazyIterable<X> {
+        return new LazyIterable(this.iterable, [...this.operations, {
+            type: IterableOperationType.FLATTEN,
+        }]);
+    }
+
     forEach<C>(
         fn: (value: T) => void,
         context?: C,
@@ -108,6 +158,6 @@ export class LazyIterable<T> implements Iterable<T> {
 
     [Symbol.iterator](): Iterator<T, any, undefined> {
         const iterator = this.iterable[Symbol.iterator]();
-        return new LazyIterableIterator(iterator, this.operations);
+        return new LazyIterableIterator(iterator, this.operations, this.start);
     }
 }
