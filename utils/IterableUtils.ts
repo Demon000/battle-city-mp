9fda3436a5658c5c765e962422f27b44f20d5e98
Cosmin Tanislav <demonsingur@gmail.com>
2022-12-03 01:36:14 +0200

    battle-city-mp: add IterableUtils

---
+++ utils/IterableUtils.ts
@@ -0,0 +1,21 @@
+export class IterableUtils {
+    static equals<T>(first: Iterable<T>, second: Iterable<T>): boolean {
+        if (first === second) {
+            return true;
+        }
+
+        for (const value of first) {
+            if (!(value in second)) {
+                return false;
+            }
+        }
+
+        for (const value of second) {
+            if (!(value in first)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
