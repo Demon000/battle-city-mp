9325addd2213c1b5229c619fb61d55dda2de84b9
Cosmin Tanislav <demonsingur@gmail.com>
2022-12-03 01:26:56 +0200

    battle-city-mp: remove enumerable decorator

--- utils/enumerable.ts
+++ utils/enumerable.ts
@@ -1,18 +1,3 @@
-export const enumerable = <T>(target: T, key: any): void => {
-    Object.defineProperty(target, key,  {
-        set(value) {
-            Object.defineProperty(this, key, {
-                value,
-                enumerable: true,
-                writable: true,
-                configurable: true,
-            });
-        },
-        enumerable: true,
-        configurable: true,
-    });
-};
-
 export const nonenumerable = <T>(target: T, key: any): void => {
     Object.defineProperty(target, key,  {
         set(value) {
