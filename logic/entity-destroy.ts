e9392facffb29aea14d143842bf6322fe571d332
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-20 01:48:46 +0200

    battle-city-mp: implement time-less automatic destroy

--- logic/entity-destroy.ts
+++ logic/entity-destroy.ts
@@ -1,6 +1,7 @@
 import { AutomaticDestroyComponent } from '@/components/AutomaticDestroyComponent';
 import { SpawnTimeComponent } from '@/components/SpawnTimeComponent';
 import { WorldEntityComponent } from '@/components/WorldEntityComponent';
+import { Entity } from '@/ecs/Entity';
 import { Registry } from '@/ecs/Registry';
 
 export function destroyAllWorldEntities(registry: Registry): void {
@@ -11,12 +12,20 @@ export function destroyAllWorldEntities(registry: Registry): void {
 
 export function processAutomaticDestroy(registry: Registry): void {
     for (const entity of registry.getEntitiesWithComponent(AutomaticDestroyComponent)) {
-        const automaticDestroyTimeMs =
-            entity.getComponent(AutomaticDestroyComponent).timeMs;
-        const spawnTime =
-            entity.getComponent(SpawnTimeComponent).value;
-        if (Date.now() - spawnTime > automaticDestroyTimeMs) {
-            entity.destroy();
+        const spawnTimeComponent = entity.findComponent(SpawnTimeComponent);
+        if (spawnTimeComponent !== undefined) {
+            const automaticDestroyTimeMs =
+                entity.getComponent(AutomaticDestroyComponent).timeMs;
+            const spawnTime = spawnTimeComponent.value;
+            if (Date.now() - spawnTime < automaticDestroyTimeMs) {
+                continue;
+            }
         }
+
+        entity.destroy();
     }
+}
+
+export function addAutomaticDestroy(entity: Entity): void {
+    entity.upsertComponent(AutomaticDestroyComponent);
 }
